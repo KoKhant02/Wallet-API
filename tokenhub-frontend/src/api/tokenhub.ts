e1ae426f7f5cd6ac614aaeca3b37e07f76adf64e
KoKhant02 <ko.khant.edusocietalbiz8@gmail.com>
2025-05-14 18:49:49 +0700

    deployment

--- tokenhub-frontend/src/api/tokenhub.ts
+++ tokenhub-frontend/src/api/tokenhub.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // adjust if your backend runs on another port
+  baseURL: 'https://token-hub.onrender.com/api', // adjust if your backend runs on another port
 });
 
 export default api;
